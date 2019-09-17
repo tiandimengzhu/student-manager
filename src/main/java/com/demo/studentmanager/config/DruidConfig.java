8319823dd0b2aeeea0f92435dfb68098759568c0
tiandimengzhu <tiandimengzhu@users.noreply.github.com>
2019-09-17 09:31:18 +0800

    init config

---
+++ src/main/java/com/demo/studentmanager/config/DruidConfig.java
@@ -0,0 +1,28 @@
+package com.demo.studentmanager.config;
+
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+/**
+ * Druid配置
+ *
+ * @author huzongtao
+ */
+@Configuration
+@MapperScan(basePackages = "com.demo.studentmanager.dao")
+@EnableTransactionManagement
+public class DruidConfig {
+    @Bean
+    @Primary
+    @ConfigurationProperties("spring.datasource.druid")
+    public DataSource dataSource(){
+        return DruidDataSourceBuilder.create().build();
+    }
+}
